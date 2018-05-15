6e104c99bef109bc13ee78d53842df73cf55fa0b
Harry du Rails Heroes Course <harry.jmg@gmail.com>
2018-05-15 07:44:02 +0000

    Article edit ok

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     def create
         @article = Article.new(article_params)
         if @article.save
@@ -13,6 +17,16 @@ class ArticlesController < ApplicationController
             render 'new'
         end
         
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article bel et bien modifié"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
     end
     
     def show

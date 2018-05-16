4b7077e60bcd3d2e0c13970a7f044727862edf40
Harry du Rails Heroes Course <harry.jmg@gmail.com>
2018-05-16 04:54:23 +0000

    Add destroy action and some partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,6 +37,13 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article supprimé"
+        redirect_to articles_path
+    end
+    
     private
     
     def article_params

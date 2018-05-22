9a2d9efd3aac5c859445464a6dad9e50cf9f02c3
Harry du Rails Heroes Course <harry.jmg@gmail.com>
2018-05-22 04:49:49 +0000

    flash messages and validations errors stylized

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article bel et bien créé"
+            flash[:success] = "Article bel et bien créé"
             redirect_to article_path(@article)
         else
             render 'new'
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
     
     def update
         if @article.update(article_params)
-            flash[:notice] = "Article bel et bien modifié"
+            flash[:success] = "Article bel et bien modifié"
             redirect_to article_path(@article)
         else
             render 'edit'
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
     
     def destroy
         @article.destroy
-        flash[:notice] = "Article supprimé"
+        flash[:danger] = "Article supprimé"
         redirect_to articles_path
     end
     

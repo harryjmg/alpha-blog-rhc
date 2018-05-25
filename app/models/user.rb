38c91664e4cc95d182d31ed9cca332c91073fb3b
Harry du Rails Heroes Course <harry.jmg@gmail.com>
2018-05-25 05:29:55 +0000

    retry of session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ActiveRecord::Base
-    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 25}
-    VALID_EMAIL_REGEX = /.+\@.+\..+/i
-    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+    
 end

65f90928636a9ad65f2dd3ee6cfd898ff00a1f3a
Harry du Rails Heroes Course <harry.jmg@gmail.com>
2018-05-28 07:01:46 +0000

    add password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@ class User < ActiveRecord::Base
     validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
+                    
+    has_secure_password
 end

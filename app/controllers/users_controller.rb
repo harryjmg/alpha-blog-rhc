c6f8a62ea08cbb504b63752317cffd63f9dde32f
Harry du Rails Heroes Course <harry.jmg@gmail.com>
2018-06-01 09:10:33 +0000

    authentication method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show]
+    
     def index
         @users = User.paginate(:page => params[:page], :per_page => 5)
     end
@@ -18,11 +20,9 @@ class UsersController < ApplicationController
     end
     
     def edit
-        @user = User.find(params[:id])
     end
     
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "User mis a jour avec succes !"
             redirect_to articles_path
@@ -32,11 +32,13 @@ class UsersController < ApplicationController
     end
     
     def show
-        @user = User.find(params[:id])
         @user_articles = @user.articles.paginate(:page => params[:page], :per_page => 5)
     end
     
     private
+    def set_user
+        @user = User.find(params[:id])
+    end
     
     def user_params
         params.require(:user).permit(:username, :email, :password) 
